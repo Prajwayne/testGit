0a37a430cd9539db2cb26fc5bb938c4b1216eadb
Prajwayne <46771166+Prajwayne@users.noreply.github.com>
2019-01-17 15:57:05 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 13:50:53 2018
+
+@author: Tapas
+"""
+
+#var1="hello world"
+#var2='python programming'
+#
+#
+#print(var1)
+#print(var2)
+#print ("var1[0] :",var1[0])
+#print ("var2[1:5] :",var2[1:5])
+#print("var2[-1:-6]",var2[-6:-1])
+#
+#print ("var2[:5]",var2[:5])
+#print("var2[5:]",var2[5:])
+
+#UPDATING STRING
+#var1="hello world"
+#
+#var1[0]='zq'       #the characters of that string which is olrdy assigned cannot change further
+#                   #bt the whole value can be change
+#print(var1)
+#
+#a="cheese"
+#
+#if "s" in a:
+#    print(1)
+#else:
+#    print(0)    
+#    
+
+#a="cheese"
+#print('g' not in a) same as in also
+
+#
+#print( "this prints" r"\n" " everything")
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
